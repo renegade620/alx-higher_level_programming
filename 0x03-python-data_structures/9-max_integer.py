00e8bd3e1dfdc12854803823fdac9d503e51c78b
Omondi Were <franklinegift@gmail.com>
2022-06-06 18:28:48 -0700

    max-integer

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list:
+        x = my_list[0]
+        for i in my_list:
+            if i > x:
+                x = i
+            else:
+                return None
+    return x
