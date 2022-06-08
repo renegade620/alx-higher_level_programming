9ba00987ecbbbabc2b4da417c89430b89fba1122
Omondi Were <franklinegift@gmail.com>
2022-06-08 15:32:12 -0700

    calc

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import sys
+
+from calculator_1 import add, sub, mul, div
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    if sys.argv[2] not in ['+', '-', '*', '/']:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    a, op, b = sys.argv[1:]
+
+    if op == '+':
+        func = add
+    elif op == '-':
+        func = sub
+    elif op == '*':
+        func = mul
+    else:
+        func = div
+
+    print("{} {} {} = {:d}".format(a, op, b, func(int(a), int(b))))
