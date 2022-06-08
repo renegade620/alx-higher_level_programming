143ffe2ab8d9e38e42a0db987aeca17926056441
Omondi Were <franklinegift@gmail.com>
2022-06-08 14:55:48 -0700

    arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,6 @@ if __name__ == "__main__":
     a = len(sys.argv)
     b = "b" if a == 1 or a > 2 else ""
     c = ":" if a > 1 else "."
-    
     print("{:d} argument{}{}".format(a - 1, b, c))
 
     for i in range(1, a):
