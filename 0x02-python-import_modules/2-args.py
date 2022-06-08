2a46a23a162fa1c29246abefe52a959f0cdf3f37
Omondi Were <74902740+renegade620@users.noreply.github.com>
2022-06-09 00:54:32 +0300

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,18 +4,10 @@ import sys
 if __name__ == "__main__":
 
     a = len(sys.argv)
+    b = "b" if a == 1 or a > 2 else ""
+    c = ":" if a > 1 else "."
     
-    if a == 1 or a > 2:
-        b = "b"
-    else:
-        ""
+    print("{:d} argument{}{}".format(a - 1, b, c))
 
-    if a > 1:
-        c = ":"
-    else:
-        "."
-
-        print("{:d} argument{}{}".format(a - 1, b, c))
-
-        for i in range(1, a):
-            print("{:d}: {}".format(i, sys.argv[i]))
+    for i in range(1, a):
+        print("{:d}: {}".format(i, sys.argv[i]))
