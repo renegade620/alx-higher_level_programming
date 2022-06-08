97336a52e735534cb501c4dcf6cf34e95c4fa595
Omondi Were <74902740+renegade620@users.noreply.github.com>
2022-06-09 01:59:00 +0300

    Update 102-magic_calculation.py

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from magic_calculation_102 import add, sub
+
+
+def magic_calculation(a, b):
+    if (x < y):
+        z = add(x, y)
+
+        for i in range(4, 6):
+            z = add(z, i)
+
+        return z
+    else:
+        return sub(x, y)
