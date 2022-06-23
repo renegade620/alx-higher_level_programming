857a1c7b77541a3435dc7ca91eb404be543fc7ce
Omondi Were <franklinegift@gmail.com>
2022-06-23 05:30:19 -0700

    square size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Square size
+
+"""
+
+
+class Square:
+    """defines a square"""
+
+    def __init__(self, size):
+        """
+        initialize square
+
+        Args: size - size of square
+
+        """
+        self.__size = size
