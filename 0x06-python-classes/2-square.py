0283ba74fe967000ee696d224ece590c44f328b1
Omondi Were <franklinegift@gmail.com>
2022-06-23 11:41:15 -0700

    square

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -14,8 +14,7 @@ class Square:
         """
         if type(size) is not int:
             raise TypeError("size must be an integer")
-        
         if size < 0:
             raise ValueError("size must be >= 0")
-    
+
         self._size = size
