c83728c17659d84951dcf3b698c5647be8e94016
Omondi Were <franklinegift@gmail.com>
2022-06-27 13:02:18 -0700

    deletion

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -4,6 +4,9 @@
 
 class Rectangle:
     """ defines a rectangle """
+
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """init
 
@@ -13,6 +16,7 @@ class Rectangle:
         """
         self.width = width
         self.height = height
+        type(self).number_of_instances += 1
 
     @property
     def width(self):
