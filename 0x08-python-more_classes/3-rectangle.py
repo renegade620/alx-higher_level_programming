eb3cd2a8d1a9ede92c2bfcf6727fddd4dfe11555
Omondi Were <franklinegift@gmail.com>
2022-06-29 19:58:57 -0700

    str

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -47,7 +47,7 @@ class Rectangle:
     @def perimeter(self):
         """ rectangle perimeter """
         if self.__width == 0 or self.__height == 0:
-            return 0
+            return (0)
         return (2 * self.__width) + (2 * self.__height)
 
     def __str__(self):
