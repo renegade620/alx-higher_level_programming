9c558a1bac13b77d364bbc241a2f1243b5c10850
Omondi Were <franklinegift@gmail.com>
2022-07-04 11:19:36 -0700

    test

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" matrix division """
+
+
+def matrix_divided(matrix, div):
+    """ divide matrix elements
+
+    Args:
+        matrix: list of lists of integers or floats
+        div: the dividing number
+
+    Returns: new matrix
+
+    """
+    if type(matrix) is not list or matrix == []
