178714f2df976d1fb60cdefb2392f7f185fd6411
Omondi Were <74902740+renegade620@users.noreply.github.com>
2022-08-11 22:22:58 +0300

    Update 1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,7 +1,3 @@
 -- create user with all privileges
-DROP USER IF EXISTS 'user_0d_1'@'localhost';
-CREATE USER 
-  IF NOT EXISTS user_0d_1@localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES 
-  ON * . * TO user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-FLUSH PRIVILEGES;
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
